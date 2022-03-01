e2f747808448a3742d9b9cee2a23c70b3e94867d
nmemarcoding <nmemarcoding@gmail.com>
2022-03-01 10:18:55 -0800

    3

---
+++ public/javascript/comment.js
@@ -0,0 +1,18 @@
+if (comment_text) {
+    const response = await fetch('/api/comments', {
+        method: 'POST',
+        body: JSON.stringify({
+            post_id,
+            comment_text
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
+
+    if (response.ok) {
+        document.location.reload();
+    } else {
+        alert(response.statusText);
+    }
+}
